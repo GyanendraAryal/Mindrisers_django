3f2efedf4840f6e75aa380acfb31795947b93872
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-07-12 20:46:03 +0545

    feat: update TodoList model and migrations

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class TodoList(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.BooleanField(null=True, blank=False)
+
+
+# models.py -> migration_file -> database_table
+# create_class makemigrations migrate
