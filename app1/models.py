85d630a6164b6cc232c53435074bfc05675ca50f
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-07-14 22:33:36 +0545

    Update models and migrations

--- app1/models.py
+++ app1/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
+# models.py -> migration_file -> database_table
+# create_class makemigrations migrate
+
+
+# class TodoList(models.Model):
+#     title = models.CharField(max_length=100)
+#     description = models.TextField()
+#     status =models.BooleanField(default=False)
+#     date= models.DateField()
+
 
 # Create your models here.
 class TodoList(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    status = models.BooleanField(null=True, blank=False)
+    status = models.BooleanField(default=False)
+    # date = models.DateField()
 
 
-# models.py -> migration_file -> database_table
-# create_class makemigrations migrate
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField(null=True)
+
+    def __str__(self):
+        return self.title
